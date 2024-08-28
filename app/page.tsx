a90eaf1d2ae0506dba2f94709071279fcfebf755
Saad Equbal <90771672+ssequbal@users.noreply.github.com>
2024-08-28 16:41:37 -0230

    check

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,6 @@ export default function Home() {
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
         <Hero/>
-        
         <Testimonials></Testimonials>
         <TextGenerateEffect   className='text-4xl' words='Celebrating Winners & Awards'></TextGenerateEffect>
         <div className='items-center fade-in'>
