cb8fa35378438b5d605e3eb29bdbc4a7ea15d01a
Saad Equbal <90771672+ssequbal@users.noreply.github.com>
2024-08-29 15:19:42 -0230

    speed

--- app/page.tsx
+++ app/page.tsx
@@ -8,12 +8,14 @@ import Introduction from "@/components/Introduction";
 import Footer from "@/components/Footer";
 import Navbar from "@/components/Navbar";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export default function Home() {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
         <Analytics></Analytics>
+        <SpeedInsights></SpeedInsights>
         <Hero/>
         <Navbar></Navbar>
         <Introduction></Introduction>
